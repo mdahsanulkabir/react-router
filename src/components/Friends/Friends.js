2a933ed045a259e6a18995cb96983d124a10afbb
ahsan <makabir98@yahoo.com>
2022-04-01 20:35:33 +0600

    some routing done with custom link

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect ( () => {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setFriends(data));
+    }, []);
     return (
         <div>
             <h2>Hello friends how are you</h2>
-            <p>lend some money</p>
+            <p>lend some money..: {friends.length}</p>
+            {
+                friends.map(friend => <Friend key={friend.id} friend={friend}></Friend>)
+            }
         </div>
     );
 };
